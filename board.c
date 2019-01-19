8788787af262f0760efcab7edd0894bb05f46e62
Max Baird <mmb1@hw.ac.uk>
2019-01-19 12:34:36 +0000

    Added comments

--- board.c
+++ board.c
@@ -4,31 +4,49 @@
 #include "tile.h"
 
 static void setPaths(Board *b, Tile *t){
+  /* Copy values to local variables for readiblity later on */
   int height = b->dimension.height;
   int width = b->dimension.width;
 
   int X = t->coordinate.x;
   int Y = t->coordinate.y;
 
-  size_t idx = (height * Y) + X;
+  size_t idx = (height * Y) + X; // Calculate the index of the current tile
 
-  int N = idx - width;
-  int S = idx + height;
-  int E = idx + 1;
-  int W = idx - 1;
-  int NE = idx - (width - 1);
-  int SE = idx + (width + 1);
-  int SW = idx + (width - 1);
-  int NW = idx - (width + 1);
+  /* Since the board is perfect square of 4x4 tiles the index of the adjacent
+   * tiles can be calculated by moving forward and backward by fixed amoutns
+   * from the current tile.
+   *
+   * This method will of course generate invalid tile positions, so the
+   * positions calculated here are verified before being assigned.
+   */
+  int N_idx = idx - width;
+  int S_idx = idx + height;
+  int E_idx = idx + 1;
+  int W_idx = idx - 1;
+  int NE_idx = idx - (width - 1);
+  int SE_idx = idx + (width + 1);
+  int SW_idx = idx + (width - 1);
+  int NW_idx = idx - (width + 1);
 
-  t->N = (N < 0) ? NULL : &b->tiles[N];
-  t->S = (Y+1 >= height) ? NULL : &b->tiles[S];
-  t->E = (X+1 >= width) ? NULL : &b->tiles[E];
-  t->W = (W < 0) ? NULL : &b->tiles[W];
-  t->NE = (N < 0 || X+1 >= width) ? NULL : &b->tiles[NE];
-  t->SE = (Y+1 >= height || X+1 >= width) ? NULL : &b->tiles[SE];
-  t->SW = (Y+1 >= height || W < 0) ? NULL : &b->tiles[SW];
-  t->NW = (N < 0 || W < 0) ? NULL : &b->tiles[NW];
+  /* The next block of code does bounds checking for calculated tile indexes.
+   * The calculated tile index must fall within the dimensions of the grid
+   * before it is assigned to the cardinal position of the current tile.
+   *
+   * If an index falls outside of the grid, it is invalid and the corresponding
+   * cardinal position is set to NULL.
+   *
+   * If the index is valid, the current cardinal position of the tile is assigned
+   * a reference to tile actually at that position.
+   */
+  t->N = (N_idx < 0) ? NULL : &b->tiles[N_idx];
+  t->S = (Y+1 >= height) ? NULL : &b->tiles[S_idx];
+  t->E = (X+1 >= width) ? NULL : &b->tiles[E_idx];
+  t->W = (W_idx < 0) ? NULL : &b->tiles[W_idx];
+  t->NE = (N_idx < 0 || X+1 >= width) ? NULL : &b->tiles[NE_idx];
+  t->SE = (Y+1 >= height || X+1 >= width) ? NULL : &b->tiles[SE_idx];
+  t->SW = (Y+1 >= height || W_idx < 0) ? NULL : &b->tiles[SW_idx];
+  t->NW = (N_idx < 0 || W_idx < 0) ? NULL : &b->tiles[NW_idx];
 }
 
 Board *makeBoard(Tile *tiles, size_t height, size_t width){
@@ -45,12 +63,10 @@ Board *makeBoard(Tile *tiles, size_t height, size_t width){
 
   size_t i = 0;
   size_t j = 0;
-  //size_t idx = 0;
   Tile *currentTile = tiles;
 
   for(i = 0; i < height; i++){
     for(j = 0; j < width; j++){
-      //idx = (width * i) + j;
       currentTile->coordinate.x = j;
       currentTile->coordinate.y = i;
       setPaths(board, currentTile);
