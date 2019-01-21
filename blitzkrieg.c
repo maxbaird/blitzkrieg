d7da4a2b64b76ef88f324e669eac992020cd0fc9
Max Baird <mmb1@hw.ac.uk>
2019-01-21 23:08:56 +0000

    Added main execution loop that reads input from the command line

--- blitzkrieg.c
+++ blitzkrieg.c
@@ -8,55 +8,52 @@
 
 #define HEIGHT 4
 #define WIDTH 4
-
-void printTile(Tile *t){
-  fprintf(stdout, "(%u,%u): %c\n", t->coordinate.x, t->coordinate.y, t->letter);
-
-  if(t->N != NULL)
-    fprintf(stdout, "N: %c ", t->N->letter);
-  if(t->S != NULL)
-    fprintf(stdout, "S: %c ", t->S->letter);
-  if(t->E != NULL)
-    fprintf(stdout, "E: %c ", t->E->letter);
-  if(t->W != NULL)
-    fprintf(stdout, "W: %c ", t->W->letter);
-  if(t->NE != NULL)
-    fprintf(stdout, "NE: %c ", t->NE->letter);
-  if(t->SE != NULL)
-    fprintf(stdout, "SE: %c ", t->SE->letter);
-  if(t->SW != NULL)
-    fprintf(stdout, "SW: %c ", t->SW->letter);
-  if(t->NW != NULL)
-    fprintf(stdout, "NW: %c ", t->NW->letter);
-
-  fprintf(stdout, "\n");
-}
+#define MAX_LETTERS 16
+#define PROMPT ">> "
 
 //heh heh heh...
+
+static void blitzkrieg(Board *board){
+  char letters[MAX_LETTERS+1] = {'\0'};
+  char *res = NULL;
+  char *p = NULL;
+  int c = 0;
+
+  for(;;){
+    fprintf(stdout, "\n%s", PROMPT);
+
+    res = fgets(letters, MAX_LETTERS+1, stdin);
+
+    //Replace newline with terminator
+    if((p=strchr(letters, '\n')) != NULL){
+      *p = '\0';
+    }
+
+    if(res == NULL){break;}
+
+    if(strlen(letters) != MAX_LETTERS){
+      fprintf(stderr, "%d letters needed; %zu entered!\n", MAX_LETTERS, strlen(letters));
+      continue;
+    }
+
+    placeLetters(board, letters);
+    findWords();
+
+    //Consume newline that causes next call to fgets to skip
+    while((c = getchar()) != '\n' && c != EOF);
+  }
+}
 
 int main(){
   Tile *tiles = makeTiles(HEIGHT * WIDTH);
   Board *board = makeBoard(tiles, HEIGHT, WIDTH);
-
-  placeLetters(board, "abcdefghijklmnop");
-
-  //size_t i = 0;
-  //for(i = 0; i < HEIGHT * WIDTH; i++){
-  //  printTile(&board->tiles[i]);
-  //}
-
   initFinder(board);
-  findWords();
+
+  blitzkrieg(board);
 
   clearTiles(tiles);
   clearBoard(board);
   unloadFinder();
-
-  //loadLexis();
-  //fprintf(stdout, "%s\n", isWord("abacus") ? "True" : "False");
-  //fprintf(stdout, "%s\n", isWord("zebra") ? "True" : "False");
-  //fprintf(stdout, "%s\n", isWord("adeawerwerzebra") ? "True" : "False");
-  //unloadLexis();
 
   return EXIT_SUCCESS;
 }
