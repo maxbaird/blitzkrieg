da4dac419ea1ffbda4e161851d9e9b2d1baafd51
Max Baird <mmb1@hw.ac.uk>
2019-01-21 17:23:49 +0000

    Cleaned up traversal.
    Modified traverse function to handle root tiles. This then allows
    the removal of multiple calls to traverse with the loop iterating over
    all tiles. No memory leaks were found.

--- finder.c
+++ finder.c
@@ -5,9 +5,8 @@
 #include "board.h"
 #include "lexis.h"
 
-/*static*/ bool canMove(Tile *t, int *tilePath){
+static bool canMove(Tile *t, int *tilePath){
   if(t == NULL) return false;
-  //if(!t->visited) return true;
   int i = 0;
   if(tilePath != NULL){
   for(i = 0; i < 17; i++){
@@ -33,11 +32,17 @@ static void traverse(Tile *t, char *letters, int *path){
     exit(EXIT_FAILURE);
   }
 
-  memcpy(tilePath, path, sizeof(int) * 17);
+  if(path != NULL){
+    memcpy(tilePath, path, sizeof(int) * 17);
+  }
   tilePath[t->id] = t->id;
 
-  strcpy(str, letters);
-  str[strlen(letters)] = t->letter;
+  if(letters != NULL){
+    strcpy(str, letters);
+    str[strlen(letters)] = t->letter;
+  }else{
+    str[0]=t->letter;
+  }
 
   if(isWord(str)){
     fprintf(stdout, "%s:%zu\n", str, strlen(str));
@@ -61,35 +66,15 @@ void initFinder(){
 }
 
 void findWords(Board *board){
-  //size_t numTiles = board->dimension.height * board->dimension.width;
-  //size_t i = 0;
+  size_t numTiles = board->dimension.height * board->dimension.width;
+  size_t i = 0;
 
   Tile *tile = board->tiles;
-  tile->visited=true;
 
-  char letters[2] = {tile->letter, '\0'};
-  int tilePath[] = {tile->id};
-
-  if(canMove(tile->N, NULL)){traverse(tile->N, letters, tilePath);}
-  tile->visited=true;
-  if(canMove(tile->S, NULL)){traverse(tile->S, letters, tilePath);}
-  tile->visited=true;
-  if(canMove(tile->E, NULL)){traverse(tile->E, letters, tilePath);}
-  tile->visited=true;
-  if(canMove(tile->W, NULL)){traverse(tile->W, letters, tilePath);}
-  tile->visited=true;
-  if(canMove(tile->NE, NULL)){traverse(tile->NE, letters, tilePath);}
-  tile->visited=true;
-  if(canMove(tile->SE, NULL)){traverse(tile->SE, letters, tilePath);}
-  tile->visited=true;
-  if(canMove(tile->SW, NULL)){traverse(tile->SW, letters, tilePath);}
-  tile->visited=true;
-  if(canMove(tile->NW, NULL)){traverse(tile->NW, letters, tilePath);}
-
-  fprintf(stdout, "After traversing\n");
-  //for(i = 0; i < numTiles; i++){
-    //tile++;
- // }
+  for(i = 0; i < numTiles; i++){
+    traverse(tile, NULL, NULL);
+    tile++;
+  }
 }
 
 void unloadFinder(){
