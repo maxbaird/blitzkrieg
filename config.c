a1092cf3b9158286c90232cc702c35e493e0e3ad
Max Baird <mmb1@hw.ac.uk>
2019-01-27 17:24:13 +0000

    More updates to reading configuration

--- config.c
+++ config.c
@@ -8,17 +8,7 @@
 #define FILENAME "config.conf"
 #define LINE_BUFFER 128
 
-//typedef enum type{intVal, boolVal, stringVal}Type;
-
-//typedef struct config{
-//  size_t MAX_WORD_LENGTH;
-//  size_t MIN_WORD_LENGTH;
-//  size_t MAX_WORDS_PER_ROW;
-//  size_t WORD_COLUMNS_PER_ROW;
-//  bool SORT_DESCENDING;
-//  bool REMOVE_MULTIPLE_COLUMN_DUPLICATES;
-//  char LEXIS_FILE[FILE_NAME_LENGTH];
-//}Config;
+static Config config = {9, 2, 10, 16, true, false, "lexis"};
 
 static const char* MAX_WORD_LENGTH = "MAX_WORD_LENGTH";
 static const char* MIN_WORD_LENGTH = "MIN_WORD_LENGTH";
@@ -56,7 +46,7 @@ static bool readVal(char *line, char *format, void *val){
 
   ret = sscanf(token, format, val);
 
-  if(ret < 1){
+  if(ret != 1){
     return false;
   }
 
@@ -74,31 +64,31 @@ static int getBoolean(char *str, bool *b){
   return -1;
 }
 
-static bool populateConfig(Config *config, char *line, char **err){
+static bool populateConfig(char *line, char **err){
   char str[64] = {'\0'};
   int ret = 0;
   bool successfulRead = true;
 
   if(strstr(line, MAX_WORD_LENGTH) != NULL){
-    successfulRead = readVal(line, "%zu", &config->MAX_WORD_LENGTH);
+    successfulRead = readVal(line, "%zu", &config.MAX_WORD_LENGTH);
     *err = successfulRead ? NULL : strdup(MAX_WORD_LENGTH);
   }
   if(strstr(line, MIN_WORD_LENGTH) != NULL){
-    successfulRead = readVal(line, "%zu", &config->MIN_WORD_LENGTH);
+    successfulRead = readVal(line, "%zu", &config.MIN_WORD_LENGTH);
     *err = successfulRead ? NULL : strdup(MIN_WORD_LENGTH);
   }
   if(strstr(line, MAX_WORDS_PER_ROW) != NULL){
-    successfulRead = readVal(line, "%zu", &config->MAX_WORDS_PER_ROW);
+    successfulRead = readVal(line, "%zu", &config.MAX_WORDS_PER_ROW);
     *err = successfulRead ? NULL : strdup(MAX_WORDS_PER_ROW);
   }
   if(strstr(line, WORD_COLUMNS_PER_ROW) != NULL){
-    successfulRead = readVal(line, "%zu", &config->WORD_COLUMNS_PER_ROW);
+    successfulRead = readVal(line, "%zu", &config.WORD_COLUMNS_PER_ROW);
     *err = successfulRead ? NULL : strdup(WORD_COLUMNS_PER_ROW);
   }
   if(strstr(line, SORT_DESCENDING) != NULL){
     successfulRead = readVal(line, "%s", str);
     if(successfulRead){
-      ret = getBoolean(str, &config->SORT_DESCENDING);
+      ret = getBoolean(str, &config.SORT_DESCENDING);
       if(ret != 0){
         successfulRead = false;
       }
@@ -108,7 +98,7 @@ static bool populateConfig(Config *config, char *line, char **err){
   if(strstr(line, REMOVE_MULTIPLE_COLUMN_DUPLICATES) != NULL){
     successfulRead = readVal(line, "%s", str);
     if(successfulRead){
-      ret = getBoolean(str, &config->REMOVE_MULTIPLE_COLUMN_DUPLICATES);
+      ret = getBoolean(str, &config.REMOVE_MULTIPLE_COLUMN_DUPLICATES);
       if(ret != 0){
         successfulRead = false;
       }
@@ -116,14 +106,37 @@ static bool populateConfig(Config *config, char *line, char **err){
     *err = successfulRead ? NULL : strdup(REMOVE_MULTIPLE_COLUMN_DUPLICATES);
   }
   if(strstr(line, LEXIS_FILE_NAME) != NULL){
-    successfulRead = readVal(line, "%s", &config->LEXIS_FILE_NAME);
+    successfulRead = readVal(line, "%s", &config.LEXIS_FILE_NAME);
     *err = successfulRead ? NULL : strdup(LEXIS_FILE_NAME);
   }
   return successfulRead;
 }
 
-Config getConfig(){
-  Config config = {9, 10, 16, true, "lexis"};
+static void restoreDefaultConfig(const char* value){
+  if(strcmp(value, MAX_WORD_LENGTH) == 0){
+    config.MAX_WORD_LENGTH = 9;
+  }
+  if(strcmp(value, MIN_WORD_LENGTH) == 0){
+    config.MIN_WORD_LENGTH = 2;
+  }
+  if(strcmp(value, MAX_WORDS_PER_ROW) == 0){
+    config.MAX_WORDS_PER_ROW = 10;
+  }
+  if(strcmp(value, WORD_COLUMNS_PER_ROW) == 0){
+    config.WORD_COLUMNS_PER_ROW = 16;
+  }
+  if(strcmp(value, SORT_DESCENDING) == 0){
+    config.SORT_DESCENDING = true;
+  }
+  if(strcmp(value, REMOVE_MULTIPLE_COLUMN_DUPLICATES) == 0){
+    config.REMOVE_MULTIPLE_COLUMN_DUPLICATES = false;
+  }
+  if(strcmp(value, LEXIS_FILE_NAME) == 0){
+    strcpy(config.LEXIS_FILE_NAME, "lexis");
+  }
+}
+
+void readConfig(){
   FILE *fp = fopen(FILENAME, "r");
   char line[LINE_BUFFER] = {'\0'};
   bool success = true;
@@ -132,7 +145,7 @@ Config getConfig(){
   if(fp == NULL){
     fprintf(stderr, "Failed to read config file \"%s\"\n", FILENAME);
     fprintf(stderr, "Using default configuration\n");
-    return config;
+    return;
   }
 
   while(fgets(line, LINE_BUFFER, fp) != NULL){
@@ -142,10 +155,11 @@ Config getConfig(){
       continue;
     }
 
-    success = populateConfig(&config, line, &readErr);
+    success = populateConfig(line, &readErr);
 
     if(!success){
-      fprintf(stderr, "Error reading \"%s\" config. Default value will be used.\n", readErr);
+      fprintf(stderr, "Error reading \"%s\" value. Default configuration value will be used.\n", readErr);
+      restoreDefaultConfig(readErr);
       free(readErr);
     }
   }
@@ -157,6 +171,6 @@ Config getConfig(){
 }
 
 int main(){
-  getConfig();
+  readConfig();
   return EXIT_SUCCESS;
 }
